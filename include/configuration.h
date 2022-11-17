8f7fbf600ccf5db3a6bc33e645d2cfc958b8b87f
Felipe Pasqualotto <felipe.pasqualotto@amplimed.com.br>
2022-11-17 20:33:14 -0300

    removido classe e ajustado para conectar

--- include/configuration.h
+++ include/configuration.h
@@ -4,15 +4,14 @@
 #define MONITOR_BAUDRATE 115200
 
 // WiFi Connection information
-#define WIFI_SSID "FELIPE_MHNET"
-#define WIFI_PASSWORD "fera3007"
+#define WIFI_SSID "trabprogdistribuida"
+#define WIFI_PASSWORD "unoesc123"
 #define WIFI_HOSTNAME "ESP32-MQTT"
 
 // MQTT Connection information
-#define BROKER_DNS "things.ubidots.com"
+#define BROKER_DNS "192.168.88.248"
 #define BROKER_PORT 1883
-#define MQTT_ID "614e5597981c92082b4677ec"
-#define MQTT_TOKEN "BBFF-O3o9A1Im0srGwjLfKPMa7Bas1MpDjz"
-#define MQTT_TOPIC "/v1.6/devices/abrangenteprogmovel"
+#define MQTT_TOPIC_CONTROLE_SENSOR "ControleSensor"
+#define MQTT_TOPIC_COMANDOS_SENSOR "ComandosSensor"
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,140 @@
-#include "configuration.h"
+#include "main.h"
 
-#include <WiFi.h>
-#include <PubSubClient.h>
+using namespace std;
 
-#include <MqttManager.h>
-#include <WiFiManager.h>
+bool isMQTTConnected = false;
+String commands[3] = {"1", "2", "3"};
 
-MqttManager *mqttManager;
-WiFiManager *wiFiManager;
+void connectToWiFi()
+{
+  if (WiFi.status() == WL_CONNECTED)
+    return;
 
-// Declarando funções
-void mqttCallback(char *topic, byte *payload, unsigned int length);
-void publishToMqttTopic();
+  Serial.println("Connecting to WiFi...");
+  Serial.println(WIFI_SSID);
+  Serial.println(WIFI_PASSWORD);
+  try
+  {
+    WiFi.setHostname(WIFI_HOSTNAME);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  }
+  catch (const std::exception &e)
+  {
+    Serial.println("Erro ao conectar WiFi");
+  }
+}
+
+void onWiFiEvent(WiFiEvent_t event)
+{
+  switch (event)
+  {
+  case SYSTEM_EVENT_STA_START:
+    Serial.println("WIFI: Connecting...");
+    break;
+  case SYSTEM_EVENT_STA_CONNECTED:
+    Serial.println("WIFI: Connected! Waiting for IP...");
+    break;
+  case SYSTEM_EVENT_STA_LOST_IP:
+    Serial.println("WIFI: Lost IP address...");
+    break;
+  case SYSTEM_EVENT_STA_GOT_IP:
+    Serial.println("WIFI: Got IP!");
+    Serial.print("WIFI: IP Address: ");
+    Serial.println(WiFi.localIP());
+    connectToMQTT();
+    break;
+  case SYSTEM_EVENT_STA_DISCONNECTED:
+    Serial.println("WIFI: Disconnected.");
+    mqttReconnectTimer.detach();
+    wifiReconnectTimer.once(2, connectToWiFi);
+    break;
+  default:
+    break;
+  }
+}
+
+void onMQTTConnect(bool sessionPresent)
+{
+  Serial.println("MQTT Connected!");
+  
+  isMQTTConnected = true;
+
+  mqttClient.subscribe(MQTT_TOPIC_CONTROLE_SENSOR, 0);
+}
+
+void onMQTTDisconnect(AsyncMqttClientDisconnectReason reason)
+{
+  isMQTTConnected = false;
+  Serial.println("MQTT Disconnected...");
+  if (WiFi.isConnected())
+  {
+    mqttReconnectTimer.once(10, connectToMQTT);
+  }
+}
+
+void onMQTTMessage(
+    char *topic, char *payload,
+    AsyncMqttClientMessageProperties properties,
+    size_t len, size_t index, size_t total)
+{
+  Serial.println("MQTT Message Received!");
+  Serial.print("Topic: ");
+  Serial.println(topic);
+  Serial.print("Payload: ");
+  Serial.println(payload);
+}
+
+void connectToMQTT()
+{
+  Serial.println("Connecting to MQTT...");
+  Serial.println("MQTT_HOST");
+  Serial.println(BROKER_DNS);
+
+  try
+  {
+    mqttClient.setClientId(WIFI_HOSTNAME);
+    mqttClient.setServer(BROKER_DNS, BROKER_PORT);
+    mqttClient.setCredentials("", "");
+    mqttClient.connect();
+  }
+  catch (const std::exception &e)
+  {
+    Serial.println("Erro ao conectar MQTT");
+    Serial.println(e.what());
+  }
+}
 
 void setup()
 {
   Serial.begin(MONITOR_BAUDRATE);
+  Serial.println();
+  Serial.println();
 
-  wiFiManager = new WiFiManager(WIFI_SSID, WIFI_PASSWORD, WIFI_HOSTNAME);
+  delay(2000);
 
-  mqttManager = new MqttManager(WIFI_SSID, WIFI_PASSWORD, BROKER_DNS, BROKER_PORT, MQTT_ID, MQTT_TOKEN, mqttCallback);
+  Serial.println("MQTT\n\n");
 
-  mqttManager->addSubscriptionTopic(MQTT_TOPIC);
+  WiFi.onEvent(onWiFiEvent);
+  mqttClient.onConnect(onMQTTConnect);
+  mqttClient.onDisconnect(onMQTTDisconnect);
+  mqttClient.onMessage(onMQTTMessage);
 
-  if (mqttManager->checkConnection())
-  {
-    publishToMqttTopic();
-  }
+  connectToWiFi();
 }
 
 void loop()
 {
-}
-
-void publishToMqttTopic()
-{
-  char payload[250];
-  sprintf(payload, "{\"%s\": %d}", "alarme", 1);
-
-  Serial.println("Publishing...");
-  Serial.println(payload);
-
-  mqttManager->publishToTopic(MQTT_TOPIC, payload);
-}
-
-void mqttCallback(char *topic, byte *payload, unsigned int length)
-{
-  Serial.print("Mensagem recebida: ");
-  Serial.println(topic);
-
-  char message;
-
-  for (int i = 0; i < length; i++)
+  if (isMQTTConnected && Serial.available())
   {
-    char c = (char)payload[i];
-    message += c;
+    Serial.println("Digite o comando: ");
+    String command = Serial.readStringUntil('\n');
+
+    if (commands->indexOf(command) != -1)
+    {
+      char payload[2];
+      sprintf(payload, "%s", command.c_str());
+      mqttClient.publish(MQTT_TOPIC_COMANDOS_SENSOR, 2, true, payload);
+    }
   }
-  Serial.print("[MQTT] Mensagem recebida: ");
-  Serial.print(message);
+  delay(200);
 }
